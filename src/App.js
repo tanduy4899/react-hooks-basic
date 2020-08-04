3a54afc5b2dd0dbf7099357bd747266f969d1893
duynt <tanduy899@gmail.com>
2020-08-04 16:23:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1>welcome</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
